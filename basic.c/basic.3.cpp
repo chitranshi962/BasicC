26f5e147f39e06ddb86ccbb922195e116003992b
chitranshi shrivastavaa <129734533+chitranshi962@users.noreply.github.com>
2023-08-03 07:20:11 +0530

    Add files via upload

---
+++ basic.c/basic.3.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	a = 7;
+	b = 7;
+	printf("a == b  = %d\n",a==b);
+	printf("a && b  = %d\n",a &&b);
+	printf("a || b  = %d\n",a||b);
+	printf("a ! b  = %d\n",!b);
+	
+	
+}
+
+
---
+++ basic.c/basic2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	a =3;
+	b =5;
+
+printf("a +b = %d\n",a+b);
+printf("a -b = %d\n",a-b);
+printf("a *b = %d\n",a*b);
+printf("a %b = %d\n",a%b);
+return 0;
+	
+}
+
